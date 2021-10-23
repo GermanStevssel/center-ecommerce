2dda2e8a87cdee0828b9fd5c3f8d99c24e756c8e
GermanStevssel <germancstevssel@gmail.com>
2021-10-22 21:57:16 -0300

    agregar formateador de precios

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,11 +3,21 @@ import ItemCount from "../ItemCount";
 
 const { Meta } = Card;
 
+// Formatear números a pesos argentinos
+const formatter = new Intl.NumberFormat("es-AR", {
+	style: "currency",
+	currency: "ARS",
+	minimumFractionDigits: 0,
+});
+
 const Item = (props) => {
 	return (
 		<div>
 			<Card hoverable cover={<img alt={props.item.alt} src={props.item.img} />}>
-				<Meta title={props.item.name} description={props.item.price} />
+				<Meta
+					title={props.item.name}
+					description={formatter.format(props.item.price)}
+				/>
 				<ItemCount stock={props.item.stock} />
 			</Card>
 		</div>
