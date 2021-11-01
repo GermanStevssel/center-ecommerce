2ca6c07f4ef46a8c9d0648ef3d922d427b896e4d
GermanStevssel <germancstevssel@gmail.com>
2021-10-31 23:36:05 -0300

    desinstalar sass

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Layout } from "antd";
 import { Header } from "antd/lib/layout/layout";
 import logo from "./img/logo/logo-xs-n.png";
@@ -8,16 +9,24 @@ import ItemDetailContainer from "./components/ItemDetailContainer";
 
 function App() {
 	return (
-		<Layout>
-			<Header>
-				<div className="logo">
-					<img src={logo} alt="Logo de Center" />
-				</div>
-				<NavBar />
-			</Header>
-			<ItemListContainer contTitle="Consolas" />
-			<ItemDetailContainer />
-		</Layout>
+		<BrowserRouter>
+			<Layout>
+				<Header>
+					<div className="logo">
+						<img src={logo} alt="Logo de Center" />
+					</div>
+					<NavBar />
+				</Header>
+				<Switch>
+					<Route exact path="/">
+						<ItemListContainer contTitle="Consolas" />
+					</Route>
+					<Route path="/category/:productId">
+						<ItemDetailContainer />
+					</Route>
+				</Switch>
+			</Layout>
+		</BrowserRouter>
 	);
 }
 
