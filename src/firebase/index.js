914ee2726ff39e9ec5eeb11839f521ccc3460ad1
GermanStevssel <germancstevssel@gmail.com>
2021-12-04 20:20:11 -0300

    hide fb

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCQrULcOEypjr5Kx0gILywjvylYTFLe7GQ",
-	authDomain: "center-ecommerce.firebaseapp.com",
-	projectId: "center-ecommerce",
-	storageBucket: "center-ecommerce.appspot.com",
-	messagingSenderId: "487037391029",
-	appId: "1:487037391029:web:57faf51233dd93dbfb6efc",
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
